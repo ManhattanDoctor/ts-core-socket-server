825fc53d7487a2939bc0c35249ad6da9fa890cf3
Renat Gubaev <gubaev.r@n-t.io>
2023-08-16 17:26:28 +0300

    auto commit

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,3 +4,5 @@ export * from './transport/TransportSocket';
 export * from './transport/TransportSocketServer';
 export * from './transport/TransportSocketEventHandler';
 export * from './transport/TransportSocketCommandHandler';
+// 
+export * from './transport/handler/TransportSocketRoomHandler';
